a5636add27c91d938ce5500ed570ef59c70ffaeb
yuravariat <yura.variat@gmail.com>
2015-12-29 00:06:34 +0200

    another fix

--- RandomForestExplorer/RandomForests/RandomTreeBuilder.cs
+++ RandomForestExplorer/RandomForests/RandomTreeBuilder.cs
@@ -65,18 +65,21 @@ namespace RandomForestExplorer.RandomForests
         /// <param name="node"></param>
         /// <param name="instances"></param>
         /// <param name="randomFeaturesNum"></param>
-        private TreeNode CreateNode(List<Instance> instances)
+        private TreeNode CreateNode(List<Instance> instances, Tuple<double, string> giniPrevScore=null)
         {
             TreeNode node = new TreeNode();
             node.Item = new DecisionNode();
 
             // stop condition
-            var giniScore = GiniScore(instances);
+            if (giniPrevScore == null)
+            {
+                giniPrevScore = GiniScore(instances);
+            }
 
             // Gini score of the group is 0, the group is pure. or there is too few instances 
-            if (giniScore.Item1==0 || instances.Count <= 20)
+            if (giniPrevScore.Item1==0 || instances.Count <= 20)
             {
-                node.Item.Clacification = giniScore.Item2;
+                node.Item.Clacification = giniPrevScore.Item2;
                 return node;
             }
 
@@ -103,7 +106,8 @@ namespace RandomForestExplorer.RandomForests
                 totalCalssesS1.Add(classe, 0);                                            
                 totalCalssesS2.Add(classe, 0);                                            
             }
-            int totalSubset1, totalSubset2 = 0;
+            int totalSubset1, totalSubset2;
+            Tuple<double,string> min_gini1 = null, min_gini2 = null;
 
             // Loop thought all feature and all values.
             foreach (Feature feature in features)
@@ -152,6 +156,8 @@ namespace RandomForestExplorer.RandomForests
                         minScore = totalScore;
                         splitValue = instances[i].Values[featureIndex];
                         minScoreFeature = feature;
+                        min_gini1 = gini1;
+                        min_gini2 = gini2;
                     }
 
                     //swSplit.Stop();
@@ -163,9 +169,9 @@ namespace RandomForestExplorer.RandomForests
 
             // There is no improvment no need to split.
             // Yura: not sure about this
-            if (minScore >= giniScore.Item1)
+            if (minScore >= giniPrevScore.Item1)
             {
-                node.Item.Clacification = giniScore.Item2;
+                node.Item.Clacification = giniPrevScore.Item2;
                 return node;
             }
 
@@ -177,8 +183,8 @@ namespace RandomForestExplorer.RandomForests
             // Create right and left nodes
             var subsetLeft = instances.Where(i => i.Values[minScoreFeature.ID - 1] <= splitValue).ToList();
             var subsetRight = instances.Where(i => i.Values[minScoreFeature.ID - 1] > splitValue).ToList();
-            node.Left = CreateNode(subsetLeft);
-            node.Right = CreateNode(subsetRight);
+            node.Left = CreateNode(subsetLeft, min_gini1);
+            node.Right = CreateNode(subsetRight, min_gini2);
             node.Right.Parent = node;
             node.Left.Parent = node;
 
@@ -193,11 +199,16 @@ namespace RandomForestExplorer.RandomForests
         private Tuple<double,string> GiniScore(Dictionary<string, int> classesCounts)
         {
             double sum = 0d;
-            foreach(string classe in classesCounts.Keys)
+            double totalInst = 0;
+            foreach (string classe in classesCounts.Keys)
+            {
+                totalInst += classesCounts[classe];
+            }
+            foreach (string classe in classesCounts.Keys)
             {
                 if (classesCounts[classe] > 0)
                 {
-                    double frefrequency = (((double)classesCounts[classe]) / ((double)classesCounts.Sum(v => v.Value)));
+                    double frefrequency = (((double)classesCounts[classe]) / totalInst);
                     sum += frefrequency * frefrequency;
                 }
             }
