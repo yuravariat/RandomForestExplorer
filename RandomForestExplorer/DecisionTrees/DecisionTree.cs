a6f587c23569c386ea2875017c0d4afcf6461972
yuravariat <yura.variat@gmail.com>
2015-12-30 23:48:52 +0200

    ClassifyInstance function

--- RandomForestExplorer/DecisionTrees/DecisionTree.cs
+++ RandomForestExplorer/DecisionTrees/DecisionTree.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RandomForestExplorer.Data;
+using System;
 
 namespace RandomForestExplorer.DecisionTrees
 {
@@ -7,5 +8,25 @@ namespace RandomForestExplorer.DecisionTrees
     {
         public TreeOutput OutputType;
         public TreeNode RootNode;
+        public string ClassifyInstance(ClassificationInstance inst, TreeNode node = null)
+        {
+            if (node == null)
+            {
+                node = RootNode;
+            }
+            if (node.IsLeaf)
+            {
+                return node.Item.Clacification;
+            }
+            string feature = node.Item.SplitFeature;
+            if(inst.Values[inst.FeaturesIndexes[feature]] > node.Item.SplitValue)
+            {
+                return ClassifyInstance(inst, (TreeNode)node.Right);
+            }
+            else
+            {
+                return ClassifyInstance(inst, (TreeNode)node.Left);
+            }
+        }
     }
 }
