691d9dc4b084bf594a09ed5a180356d8ad79f050
Alex Yarovoy <yarovoy.alex@gmail.com>
2016-01-04 10:59:05 +0200

    Renamed instance value and removed odd class.

--- RandomForestExplorer/Data/InstanceValue.cs
+++ RandomForestExplorer/Data/InstanceValue.cs
@@ -4,12 +4,6 @@ using System.Collections.Generic;
 
 namespace RandomForestExplorer.Data
 {
-    public class ClassificationInstance : Instance
-    {
-        public ConcurrentDictionary<String, int> ClassCounter { get; set; }
-        public Dictionary<String, int> FeaturesIndexes { get; set; }
-    }
-
     public class InstanceValue
     {
         public ConcurrentDictionary<string,int> ClassVotes { get; set; }
